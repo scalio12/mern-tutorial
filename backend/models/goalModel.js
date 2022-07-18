dda8bc6906fac43600b7b7dcd3fe3229ec3bb9a4
scalio12 <191240000879@unisnu.ac.id>
2022-07-18 19:39:09 +0700

    initial commit

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,14 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const goalSchema = mongoose.Schema(
-    {
-        text: {
-            type: String,
-            required: [true, 'Please add a text value']
-        },
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Users",
     },
-    {
-        timestamps: true,
-    }
-)
+    text: {
+      type: String,
+      required: [true, "Please add a text value"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Goals', goalSchema)
+module.exports = mongoose.model("Goals", goalSchema);
